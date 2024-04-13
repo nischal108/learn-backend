59a039a2d466f4e6d4257ea04111c7561e19f6f7
Nischal Bhattarai <nishchalb21@gmail.com>
2024-04-13 08:50:04 +0545

    file system

--- file-server/index.js
+++ file-server/index.js
@@ -1,6 +1,5 @@
 /**
   You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-  - Use built in Node.js `fs` module
   The expected API endpoints are defined below,
   1. GET /files - Returns a list of files present in `./files/` directory
     Response: 200 OK with an array of file names in JSON format.
@@ -10,13 +9,15 @@
      Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
      Example: GET http://localhost:3000/file/example.txt
     - For any other route not defined in the server return 404
-    Testing the server - run `npm run test-fileServer` command in terminal
  */
     const express = require('express');
     const fs = require('fs');
     const path = require('path');
     const app = express();
-    const PORT = 9000;
+    const PORT = 4000;
+
+
+
     
     app.get('/', (req, res) => {
         res.send("running, everything is fine");
@@ -47,27 +48,14 @@
             res.status(200).send(data);
         });
     });
-    
-    app.listen(PORT, () => {
-        console.log(`App is running at port ${PORT}`);
+
+
+
+
+    // 404 handler
+    app.use((req, res) => {
+      res.status(404).send("404 Not Found");
     });
-    
-
-
-
-    // Error handling middleware for handling 404 Not Found errors
-app.use((req, res, next) => {
-    res.status(404).send("404 Not Found");
-});
-
-// Error handling middleware for handling other errors
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Internal Server Error");
-});
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
