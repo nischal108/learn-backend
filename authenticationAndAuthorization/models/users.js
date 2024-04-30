77efd6f75379e5805b7fae043ce06b11c12e2a8a
Nischal Bhattarai <nishchalb21@gmail.com>
2024-04-30 21:33:04 +0545

    revising all

--- authenticationAndAuthorization/models/users.js
+++ authenticationAndAuthorization/models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 try {
-    mongoose.connect("mongodb+srv://nishchalb21:NB5TbGJNdghPRr5E@cluster0.0oavpu3.mongodb.net/authenticationAndAuthorization");
+    mongoose.connect("dblink.com/authenticationAndAuthorization");
 } catch (error) {
     console.log("couldn't connect to the database");
 }
